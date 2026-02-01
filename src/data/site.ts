c6e71669789ec1761939ea0c517684a93aaaa29d
onloopdev.eth <56681313+onloopdev@users.noreply.github.com>
2026-02-01 12:53:45 +0100

    Update site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,7 +6,7 @@
 export const BLUR_FADE_DELAY = 0.05;
 
 export const siteConfig = {
-  url: "https://www.zangwei.dev", // Use 'www' for vercel recommendation
-  lastUpdated: "2025.12",
+  url: "https://netaobiakor.vercel.app", // Use 'www' for vercel recommendation
+  lastUpdated: "2026.2",
   avatarUrl: "/me.png",
 } as const;
